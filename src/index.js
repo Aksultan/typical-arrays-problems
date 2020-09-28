aa5f113fccc58420786d16da32c82f9c6abe3ca7
Aksultan <aksultan919@gmail.com>
2020-09-28 22:27:20 +0600

    feat: add max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array!=undefined&&array.length?array.sort((a, b)=> a - b)[0]:0
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array!=undefined&&array.length?array.sort((a, b)=> a - b).slice(-1)[0]:0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array!=undefined&&array.length?(array.reduce((x,y)=>x+y)/array.length):0
 }
